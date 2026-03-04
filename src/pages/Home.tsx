bed3ff486dd0aea6189d9ad2decbf0f840e93617
HSh8w35 <heidistone888@gmail.com>
2026-03-04 13:20:10 -0500

    Updated Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -98,7 +98,7 @@ function Home() {
             </div>
 
             <div className="space-y-6 md:space-y-8">
-              <h1 className="text-2xl md:text-3xl lg:text-4xl italic text-black/90 leading-relaxed px-4 font-normal" style={{ fontFamily: "'Playfair Display', serif" }}>
+              <h1 className="text-2xl md:text-3xl lg:text-4xl italic text-black/90 leading-relaxed px-4 font-normal" style={{ fontFamily: "'Monotype Corsiva', cursive" }}>
                 Seeing Possibility Where Others See Limits
               </h1>
 
