2132e139c600fa1539458221f43ca2e59d5ee84a
HSh8w35 <heidistone888@gmail.com>
2026-03-09 15:50:56 -0400

    Updated About.tsx

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -148,7 +148,7 @@ function About() {
         </div>
       </section>
 
-      <section className="py-20 bg-[#F6F1E8] px-6 lg:px-8">
+      <section className="pt-12 pb-20 bg-[#F6F1E8] px-6 lg:px-8">
         <div className="max-w-6xl mx-auto">
           <div className="text-center mb-16">
             <h2 className="text-4xl md:text-5xl font-bold text-[#8B6F47] mb-4">
