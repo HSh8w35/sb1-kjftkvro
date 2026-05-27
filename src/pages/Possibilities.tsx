50da17a31cdd8dc3ee68c5b8218986f3f6f3af3e
HSh8w35 <heidistone888@gmail.com>
2026-05-27 10:41:31 -0400

    Fix image object-position to top

--- src/pages/Possibilities.tsx
+++ src/pages/Possibilities.tsx
@@ -190,7 +190,7 @@ function Possibilities() {
               <img
                 src="/ChatGPT_Image_May_27,_2026,_10_33_09_AM.png"
                 alt="Advisory relationship meeting in an elegant setting"
-                className="w-full h-full object-cover object-center"
+                className="w-full h-full object-cover object-top"
               />
               <div className="absolute inset-0 bg-gradient-to-b from-[#1C1710]/10 via-[#1C1710]/20 to-[#1C1710]/75" />
               <div className="absolute top-5 left-8">
